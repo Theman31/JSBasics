7d80a6c5abed0659f7138818041a45c004203fc1
Eli Dery <elidery@MacBook-Pro-3.local>
2018-02-06 16:20:41 -0700

    test 5 DONE

--- arrays/countString.js
+++ arrays/countString.js
@@ -2,7 +2,18 @@
 // a string and a letter and the function will count the number
 // of occurrences of the specified letter within the string.
 
-var countString;
+function countString(string, letter) {
+  var total = 0;
+  string = string.toLowerCase();
+  letter = letter.toLowerCase();
+  for(var i = 0; i < string.length; i++) {
+    if (string.charAt(i) === letter) {
+      total++;
+    }
+  }
+  return total;
+
+};
 
 // DO NOT DELETE
 module.exports = countString;
