a02ced98b4e886e2533311bc6bc68f90f10ee105
Eli Dery <elidery@MacBook-Pro-3.local>
2018-02-08 10:40:16 -0700

    another three round complete

--- arrays/reverseString.js
+++ arrays/reverseString.js
@@ -1,6 +1,9 @@
 // Write a JavaScript function reverseString to reverse a given string.
 
-var reverseString;
+function reverseString(val) {
+  val = val.split('').reverse().join('');
+  return val;
+};
 
 // DO NOT DELETE
 module.exports = reverseString;
--- arrays/sumIt.js
+++ arrays/sumIt.js
@@ -1,7 +1,9 @@
 // Create a function called sumIt that takes in the array and returns the sum
 // of each of the values in the array.
 
-var sumIt;
+function sumIt() {
+  
+};
 
 // DO NOT DELETE
 module.exports = sumIt;
